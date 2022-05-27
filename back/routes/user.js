12c2128db0f4a55b563eb94b290c2c7311e14995
thomas <thomasliedts@gmail.com>
2022-05-27 21:07:56 +0200

    create user route

---
+++ back/routes/user.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/User');
+
+router.post('/', async(req,res) => {
+const {email, password, role} = req.body;
+try {
+    if( await User.findOne({email})) {
+        return res.status(400).json({msg:'User already exists with this email'}) 
+    }
+    const user = new User({
+        email,
+        password,
+        role,
+    });
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.json(user);
+
+} catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server erreur');  
+}
+});
