e8896b5e97009cdab9afd22c1026bb459f50da57
TheoRougier-enoki <67681656+TheoRougier-enoki@users.noreply.github.com>
2022-06-28 17:14:19 +0200

    useUser

--- goodVibesApp/App.js
+++ goodVibesApp/App.js
@@ -5,6 +5,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import useLogin from "./hook/useLogin";
+import useUser from "./hook/useUser";
 
 export default function App() {
     const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ export default function App() {
         submit,
         userEmail,
     } = useLogin()
+    const {} = useUser()
 
 
     return (
---
+++ goodVibesApp/hook/useUser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function useUser() {
+    function getUser() {
+
+    }
+
+    return {}
+}
