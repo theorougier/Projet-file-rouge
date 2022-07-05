c59ae771f1c6f5b7acc0a643f0fcb146492da783
nbinet <nathan.binet99@gmail.com>
2022-07-05 17:22:50 +0200

    add CustomInput,js

---
+++ front/src/component/CustomInput.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function CustomInput({ title, type, name, onChange, onBlur, value }) {
+  const styles = ownStyles();
+
+  return (
+    <input
+      style={styles.input}
+      type={type}
+      name={name}
+      onChange={onChange}
+      onBlur={onBlur}
+      value={value}
+      defaultValue="test"
+    />
+  );
+}
+
+const ownStyles = () => ({
+  input: {
+    color: "white",
+    background: "rgba(0, 0, 0, 0.1)",
+    height: "50px",
+    border: "none",
+    borderRadius: "20px",
+    fontSize: "18px",
+  },
+  title: { color: "white", fontSize: "40px", marginBottom: "20px" },
+});
+
+export default CustomInput;
