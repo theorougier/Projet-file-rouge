cd61cf9cc013f56e9289947e4e0175fac6b62687
nbinet <nathan.binet99@gmail.com>
2022-07-05 15:17:43 +0200

    add reset.css

--- front/src/component/PrivateRoute.js
+++ front/src/component/PrivateRoute.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({
-                            user,
-                            redirectPath = '/login',
-                            children,
-                        }) {
-    if (!user) {
-        return <Navigate to={redirectPath} replace/>;
-    }
+function ProtectedRoute({ user, redirectPath = "/login", children }) {
+  if (!user) {
+    return <Navigate to={redirectPath} replace />;
+  }
 
-    return children;
+  return children;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
