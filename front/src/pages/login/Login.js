565e3e9fedd3f8c0cb536d9f7d5d6b02843d0a03
nbinet <nathan.binet99@gmail.com>
2022-07-16 13:22:43 +0200

    add logo in loginPage

--- front/src/pages/login/Login.js
+++ front/src/pages/login/Login.js
@@ -2,11 +2,13 @@ import React from "react";
 import LoginForm from "./LoginForm";
 import { Link } from "react-router-dom";
 import OpacityCard from "../../component/OpacityCard";
+import { ReactComponent as Logo } from "../../assets/img/logo.svg";
 
 function Login({ submit }) {
   const styles = ownStyles();
   return (
     <section style={styles.parent}>
+      <Logo style={styles.logo} />
       <OpacityCard title="Connexion">
         <LoginForm submit={submit} />
         <span>Pas de compte ?</span>
@@ -19,10 +21,12 @@ function Login({ submit }) {
 const ownStyles = () => ({
   parent: {
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     height: "100%",
   },
+  logo: { maxWidth: "90%" },
 });
 
 export default Login;
