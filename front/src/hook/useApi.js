c2227f529450dbc8df1fb48bd00ea8d30feacbed
nbinet <nathan.binet99@gmail.com>
2022-07-16 11:32:02 +0200

    add useApi

---
+++ front/src/hook/useApi.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+export default function useApi() {
+  const [randomImage, setRandomImage] = useState();
+  const [randomFact, setRandomFact] = useState();
+  const [preference, setPreferences] = useState([]);
+
+  const apiList = [
+    {
+      id: 1,
+      name: "Le saviez vous ?",
+      link: "https://api.api-ninjas.com/v1/facts?limit=1",
+    },
+    {
+      id: 2,
+      name: "Annecdotes sur les chats",
+      link: "api/facts/cat",
+    },
+    {
+      id: 3,
+      name: "Images de chats",
+      link: "api/images/cat",
+    },
+    {
+      id: 4,
+      name: "Images de chiens",
+      link: "api/images/dog",
+    },
+    {
+      id: 5,
+      name: "Blagues Chuck Norris",
+      link: "api/facts/norris",
+    },
+  ];
+
+  const imageTheme = [
+    "https://i-mom.unimedias.fr/2022/03/14/chat.jpg?auto=format,compress&cs=tinysrgb",
+    "https://cdn.futura-sciences.com/buildsv6/images/wide1920/b/c/5/bc59613aac_50186179_age-chien.jpg",
+  ];
+
+  const object = {
+    keys: "1",
+    keys1: "2",
+    keys2: "3",
+  };
+
+  Object.keys(object).map(function (key, index) {
+    console.log(object[key]);
+  });
+
+  useEffect(() => {
+    axios.get("https://dog.ceo/api/breeds/image/random").then(function (resp) {
+      return setRandomImage(resp.data.message);
+    });
+    axios
+      .get("https://api.api-ninjas.com/v1/facts?limit=1", {
+        headers: { "X-Api-Key": "10Dcsd84JCi8KfPuGm4Zfg==AFqRxwfEBg4pzVVV" },
+      })
+      .then(function (resp) {
+        return setRandomFact(resp.data[0].fact);
+      });
+  }, []);
+
+  const selectPref = (e) => {
+    if (preference.find((element) => element === e)) {
+      const index = preference.indexOf(e);
+      if (index > -1) {
+        // only splice array when item is found
+        preference.splice(index, 1); // 2nd parameter means remove one item only
+      }
+    } else {
+      preference.push(e);
+    }
+    return preference;
+  };
+
+  const postPref = () => {
+    const preferencesConvert = Object.assign({}, preference);
+    SecureStore.getItemAsync("token").then((token) => {
+      axios
+        .put(
+          "http://localhost:5000/api/user/62bea178e901f0d35add143a",
+          {
+            preferences: preferencesConvert,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .catch(function (error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
+    });
+  };
+
+  return {
+    randomImage,
+    randomFact,
+    apiList,
+    preference,
+    imageTheme,
+    selectPref,
+    postPref,
+  };
+}
