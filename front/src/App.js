dbf7d32531c75f9895b58b9fcba9468a911b6af4
nbinet <nathan.binet99@gmail.com>
2022-09-11 18:02:22 +0200

    add Home in App

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,35 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import { Route, Link } from "react-router-dom";
-import Register from "./pages/register/Register";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
 import { Routes } from "react-router";
-import Profile from "./pages/profile/Profile";
-import PrivateRoute from "./component/PrivateRoute";
 import ProtectedRoute from "./component/PrivateRoute";
 import useLogin from "./hook/useLogin";
+
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 import Preferences from "./pages/preferences/Preferences";
 
 function App() {
-  const { logged, LogOut, submit, userEmail } = useLogin();
+  const { logged, setLogged, submit } = useLogin();
 
   console.log("logged ? :", logged);
 
   return (
     <Routes>
       <Route
+        path="/home"
+        element={
+          <ProtectedRoute user={true}>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
         path="/profile"
         element={
-          <ProtectedRoute user={logged}>
+          <ProtectedRoute user={true}>
             <Profile />
           </ProtectedRoute>
         }
@@ -34,7 +42,10 @@ function App() {
           </ProtectedRoute>
         }
       />
-      <Route path="/login" element={<Login submit={submit} />} />
+      <Route
+        path="/login"
+        element={<Login submit={submit} logged={logged} />}
+      />
       <Route path="/register" element={<Register submit={submit} />} />
     </Routes>
   );
